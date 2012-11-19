d3e2b009e29ffbd4f6be673855f34f21b2f84b36
Li Xin <bangerlee@gmail.com>
2012-11-20 00:48:54 +0800

    Tag.add done, using the `sadd` command of redis, object:tag couble be inserted into the redis server

---
+++ test/index.js
@@ -0,0 +1,71 @@
+
+/**
+ * Module dependencies.
+ */
+
+var rtags = require('../lib/rtags')
+  , redis = require('redis')
+  , should = require('should')
+  , tags = rtags.createTag('blogs')
+  , db = redis.createClient();
+
+var start = new Date;
+
+db.flushdb(function(){
+  tags
+    .add('linux,kernel,linus,1991,student', 1)
+    .add('memory,cpu,disk,computer,mac', 2)
+    .add('kernel,process,thread,lock', 3)
+    .add('synchronization,kernel,mac', 4);
+});
+
+function test() {
+  var pending = 0;
+ 
+  ++pending;
+  tags
+    .queryID('3')
+    .end(function(err, ids){
+      if (err) throw err;
+      ids.should.eql(['kernel', 'process', 'thread', 'lock']);  
+      --pending || done();
+    });
+ 
+  ++pending;
+  tags
+    .queryTag('linux,linus')
+    .end(function(err, ids){
+      if (err) throw err;
+      ids.should.eql(['1']);
+      --pending || done();
+    });
+
+  ++pending;
+  tags
+    .queryTag('kernel')
+    .end(function(err, ids){
+      if (err) throw err;
+      ids.should.have.length(3);
+      ids.should.include('1');
+      ids.should.include('3');
+      ids.should.include('4');
+      --pending || done();
+    });
+
+  ++pending;
+  tags
+    .queryID('2', '4')
+    .end(function(err, ids){
+      if (err) throw err;
+      ids.should.eql(['mac']);
+      --pending || done();
+    });
+}
+
+function done() {
+  console.log();
+  console.log(' tests completed in %dms', new Date - start);
+  console.log();
+  process.exit();
+}
+
